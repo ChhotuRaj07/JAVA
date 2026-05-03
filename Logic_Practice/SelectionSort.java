e1452813263f5a169a49c47ea67db8ed8d1cbd75
ChhotuRaj07 <chhotukr825412@gmail.com>
2026-05-03 23:42:50 +0530

    Learning Selection Sort With Concepts

---
+++ Logic_Practice/SelectionSort.java
@@ -0,0 +1,21 @@
+package Logic_Practice;
+
+public class SelectionSort {
+   public static void main(String[] args) {
+      int array[] = {2,8,4,7,2,9,3};
+
+      for (int i = 0; i < array.length; i++) {
+        for (int j = i+1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                // swap Value 
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] =  temp;
+            }
+        }
+      }
+      for(int i = 0; i <= array.length; i++) {
+            System.out.print(array[i]+ " ");
+      }
+   }
+}
---
+++ Logic_Practice/seletionsortCorrect.java
@@ -0,0 +1,24 @@
+package Logic_Practice;
+
+public class seletionsortCorrect {
+    public static void main(String[] args) {
+        int array[] = {9,2,3,7,1,9,4};
+
+        for (int i = 0; i < array.length; i++){
+            int min = i;
+        for (int j = i+1; j < array.length; j++){
+            if (array[j] < array[min]) {
+         // swap value because we store minum value in  the variable in min so now i can camapre a value in.
+         int temp = array[min];
+         array[min] = array[j];
+         array[j] = temp;
+
+
+            }
+         }     
+        }
+        for (int i =0; i < array.length ; i++) {
+            System.out.print(array[i] + "");
+        }
+    }
+}
